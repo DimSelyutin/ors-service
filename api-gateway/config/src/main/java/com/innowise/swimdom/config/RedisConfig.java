2bdddf8d52c3a368317166ef733c771708233a38
Dzmitry Sialiutsin <d.sialiutsin@astondevs.ru>
2025-09-15 12:33:48 +0300

    fix: error builds

---
+++ api-gateway/config/src/main/java/com/innowise/swimdom/config/RedisConfig.java
@@ -0,0 +1,4 @@
+package com.innowise.swimdom.config;
+
+public class RedisConfig {
+}
---
+++ api-gateway/service/src/main/java/com/innowise/swimdom/service/FeignClientErrorDecoder.java
@@ -0,0 +1,38 @@
+package com.innowise.swimdom.config;
+
+import com.innowise.swimdom.exception.BadRequestException;
+import com.innowise.swimdom.exception.FeignClientException;
+import com.innowise.swimdom.exception.ForbiddenException;
+import com.innowise.swimdom.exception.NotFoundException;
+import com.innowise.swimdom.exception.ServiceUnavailableException;
+import com.innowise.swimdom.exception.UnauthorizedException;
+
+import com.innowise.swimdom.service.UserServiceFallbackFactory;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Error decoder for feign.
+ */
+@Configuration
+public class FeignClientErrorDecoder implements ErrorDecoder {
+
+    @Bean
+    public UserServiceFallbackFactory userServiceFallbackFactory() {
+        return new UserServiceFallbackFactory();
+    }
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        return switch (response.status()) {
+            case 400 -> new BadRequestException("Syntax error in the request");
+            case 404 -> new NotFoundException("User not found");
+            case 401 -> new UnauthorizedException("Invalid email or password");
+            case 403 -> new ForbiddenException("The server cannot execute the request");
+            case 503 -> new ServiceUnavailableException("Service unavailable");
+            default -> new FeignClientException("Internal server error");
+        };
+    }
+}

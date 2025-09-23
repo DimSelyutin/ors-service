c8aa9d54f835b8ad54b7173670e42c782ecc35db
Dzmitry Sialiutsin <d.sialiutsin@astondevs.ru>
2025-09-23 12:21:06 +0300

    fix: comments and auth

--- api-gateway/service/src/main/java/com/innowise/swimdom/dto/AuthenticationRequestDto.java
+++ api-gateway/service/src/main/java/com/innowise/swimdom/dto/AuthenticationRequestDto.java
@@ -10,7 +10,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
  */
 @Schema(description = "Information for requesting authentication on the user service side")
 public record AuthenticationRequestDto(
+    @NotBlank(message = "Email cannot be blank.")
+    @Size(max = 100, message = "Email length cannot exceed 100 characters.")
+    @Email(message = "Email must be a valid email address.")
     String email,
+
+    @NotBlank(message = "Password cannot be blank.")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters.")
     String password) {
 
 }
--- api-gateway/service/src/main/java/com/innowise/swimdom/dto/UserEmailRequestDto.java
+++ api-gateway/service/src/main/java/com/innowise/swimdom/dto/UserEmailRequestDto.java
@@ -8,6 +8,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
  * @param email login
  */
 @Schema(description = "Information to request authentication on the user service side in case of token refresh")
-public record UserEmailRequestDto(String email) {
+public record UserEmailRequestDto(
+    @NotBlank(message = "Email cannot be blank.")
+    @Size(max = 100, message = "Email length cannot exceed 100 characters.")
+    @Email(message = "Email must be a valid email address.")
+    String email
+) {
 
 }
